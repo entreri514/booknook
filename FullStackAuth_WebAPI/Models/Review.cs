635f6538c941479ea03a3ebc0f414684860f8003
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-12-19 20:35:57 -0600

    Added Review class key

--- FullStackAuth_WebAPI/Models/Review.cs
+++ FullStackAuth_WebAPI/Models/Review.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FullStackAuth_WebAPI.Models;
+using Microsoft.Extensions.Primitives;
+using System.ComponentModel.DataAnnotations;
 
 namespace booknook_WebAPI.Models
 {
@@ -6,6 +8,11 @@ namespace booknook_WebAPI.Models
     {
         [Key]
         public int id { get; set; }
-        public string OwnerId { get; set; }
+        public string BookId { get; set; }
+
+        public string Text { get; set; }
+        public double Rating { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
     }
 }
